c133e4e5d714ec2c0d7386c9e02590347683acf0
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-12-28 15:11:35 +0100

    Block matrix inversion

--- interior_point_transportation.py
+++ interior_point_transportation.py
@@ -51,7 +51,9 @@ class TransportationProblem:
         return A @ np.diag(G) @ A.transpose()
 
     def apply_A(N: int, M: int, t):
-        """Apply A to the vector t without forming it explicitly"""
+        """
+        Apply A to the vector t without forming it explicitly
+        """
         assert t.ndim == 1
         assert t.size == N * M
         X = t.reshape((N, M))
@@ -61,7 +63,9 @@ class TransportationProblem:
         return np.concatenate((Xs, Xt))
 
     def apply_AGAt(N: int, M: int, G, t):
-        """Apply AGAt to the vector t without forming it explicitly"""
+        """
+        Apply AGAt to the vector t without forming it explicitly
+        """
         assert G.ndim == 1
         assert G.size == N * M
         assert t.size == N + M - 1
@@ -75,3 +79,41 @@ class TransportationProblem:
         ats = ts * Xs + X @ tt
         att = tt * Xt + X.transpose() @ ts
         return np.concatenate((ats, att))
+
+    def apply_AGAt_inv(N: int, M: int, G, t):
+        """
+        Apply the inverse of AGAt to the vector t without forming it explicitly, using a block LDLt decomposition.
+        Efficient if M is much smaller than N.
+        """
+        assert G.ndim == 1
+        assert G.size == N * M
+        assert t.size == N + M - 1
+        X = G.reshape((N, M))
+        Xs = X.sum(axis=1)
+        X = X[:, :-1]  # Redundant constraint removed
+        Xt = X.sum(axis=0)
+
+        # Upper diagonal block (just a diagonal matrix)
+        Ds = Xs
+        # Lower corner block
+        L = X.transpose() / Xs
+        # Lower diagonal block Dt - L Ds Lt
+        Dt = np.diag(Xt) - (L * np.expand_dims(Ds, 0)) @ L.transpose()
+
+        ts = t[:N]
+        tt = t[N:]
+
+        # Lower triangular solve
+        ats = ts
+        att = tt - L @ ts
+
+        # Diagonal solve (requires a solve for the lower block)
+        bts = ats / Ds
+        btt = np.linalg.solve(Dt, att)
+
+        # Upper triangular solve
+        ctt = btt
+        cts = bts - L.transpose() @ btt
+        ct = np.concatenate((cts, ctt))
+
+        return ct
--- test_matrices.py
+++ test_matrices.py
@@ -3,31 +3,60 @@ import unittest
 
 from interior_point_transportation import TransportationProblem
 
-params_list = [(4, 2), (2, 2), (5, 8)]
+params_list = [(4, 5), (5, 3), (5, 8)]
 
 
 class MatrixTest(unittest.TestCase):
     def test_dense_matrix(self):
+        """
+        Test the dense AGAt against the explicit result
+        """
         for N, M in params_list:
-            A = TransportationProblem.make_dense_A(N, M)
-            G = np.random.rand(N * M)
-            AGAt = TransportationProblem.make_dense_AGAt(N, M, G)
-            expected = A @ np.diag(G) @ A.transpose()
-            self.assertTrue(np.isclose(AGAt, expected).all())
+            with self.subTest(N=N, M=M):
+                A = TransportationProblem.make_dense_A(N, M)
+                G = np.random.rand(N * M)
+                AGAt = TransportationProblem.make_dense_AGAt(N, M, G)
+                expected = A @ np.diag(G) @ A.transpose()
+                self.assertTrue(np.isclose(AGAt, expected).all())
 
     def test_apply_A(self):
+        """
+        Test the implicit A against the explicit result
+        """
         for N, M in params_list:
-            A = TransportationProblem.make_dense_A(N, M)
-            x = np.random.rand(N * M)
-            Ax = TransportationProblem.apply_A(N, M, x)
-            expected = A @ x
-            self.assertTrue(np.isclose(Ax, expected).all())
+            with self.subTest(N=N, M=M):
+                A = TransportationProblem.make_dense_A(N, M)
+                x = np.random.rand(N * M)
+                Ax = TransportationProblem.apply_A(N, M, x)
+                expected = A @ x
+                self.assertTrue(np.isclose(Ax, expected).all())
 
     def test_apply_AGAt(self):
+        """
+        Test the implicit AGAt against the explicit result
+        """
         for N, M in params_list:
-            G = np.random.rand(N * M)
-            x = np.random.rand(N + M - 1)
-            AGAtx = TransportationProblem.apply_AGAt(N, M, G, x)
-            AGAt = TransportationProblem.make_dense_AGAt(N, M, G)
-            expected = AGAt @ x
-            self.assertTrue(np.isclose(AGAtx, expected).all())
+            with self.subTest(N=N, M=M):
+                G = np.random.rand(N * M)
+                x = np.random.rand(N + M - 1)
+                AGAtx = TransportationProblem.apply_AGAt(N, M, G, x)
+                AGAt = TransportationProblem.make_dense_AGAt(N, M, G)
+                expected = AGAt @ x
+                self.assertTrue(np.isclose(AGAtx, expected).all())
+
+    def test_apply_AGAt_inv(self):
+        """
+        Test the implicit AGAt^-1 against the explicit result
+        """
+        for N, M in params_list:
+            with self.subTest(N=N, M=M):
+                G = 0.01 + np.random.rand(N * M)
+                x = np.random.rand(N + M - 1)
+                AGAt_inv_x = TransportationProblem.apply_AGAt_inv(N, M, G, x)
+                AGAt = TransportationProblem.make_dense_AGAt(N, M, G)
+                expected = np.linalg.solve(AGAt, x)
+                self.assertTrue(np.isclose(AGAt_inv_x, expected).all())
+
+
+if __name__ == "__main__":
+    unittest.main()
