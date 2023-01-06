f1a0e25cfe716cd7c2379599537907cc46068d3d
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-01-06 16:07:00 +0100

    Remove unnecessary code for our version of Mehrotra

--- transportation.py
+++ transportation.py
@@ -5,66 +5,11 @@ from numpy.linalg import norm
 from scipy import sparse
 from scipy.sparse.linalg import spsolve
 
-
-def initial_point(a, b, c, use_umfpack=False):
-
-    n = a.shape[1]
-    e = np.ones((n,))
-
-    # solution for min norm(s) s.t. A'*y + s = c
-    # y =sparse.linalg.cg(a*a.T, a*c,tol=1e-7)[0]
-    y = spsolve(a * a.T, a * c, use_umfpack=use_umfpack)
-
-    # y2 =sparse.linalg.cgs(A*A.T, A*c)[0]
-    # y2 =sparse.linalg.gmres(A*A.T, A*c,)[0]
-
-    s = c - a.T * y
-
-    # solution for min norm(x) s.t. Ax = b
-    x = a.T * spsolve(a * a.T, b, use_umfpack=use_umfpack)
-    # print(c.T.dot(x))
-    # x = a.T*sparse.linalg.cg(a*a.T, b,tol=1e-7)[0]
-
-    # delta_x and delta_s
-    delta_x = max(-1.5 * np.min(x), 0)
-    delta_s = max(-1.5 * np.min(s), 0)
-
-    # delta_x_c and delta_s_c
-    pdct = 0.5 * (x + delta_x * e).dot(s + delta_s * e)
-    delta_x_c = delta_x + pdct / (np.sum(s) + n * delta_s)
-    delta_s_c = delta_s + pdct / (np.sum(x) + n * delta_x)
-
-    print(
-        f"delta_x={delta_x}\ndelta_s={delta_s}\ndelta_x_c={delta_x_c}\ndelta_s_c={delta_s_c}\n"
-    )
-    # output
-    x0 = x + delta_x_c * e
-    s0 = s + delta_s_c * e
-    y0 = y
-    return x0, y0, s0
-
-
-def newton_direction(r_b, r_c, r_x_s, a, m, n, x, s, lu, error_check=0, use_lu=True):
+def newton_direction(r_b, r_c, r_x_s, a, x, s):
 
     p1 = -r_b
     p2 = -r_c + r_x_s / x
-    rhs = np.hstack((-r_b, -r_c + r_x_s / x))
     d_2 = -np.minimum(1e16, s / x)
-    b = sparse.vstack(
-        (
-            sparse.hstack((sparse.coo_matrix((m, m)), a)),
-            sparse.hstack((a.T, sparse.diags([d_2], [0]))),
-        )
-    )
-
-    # dy       0    A           p1
-    #     =                x     
-    # dx       At   d_2         p2
-
-
-    # dx       d_2    At        p2
-    #     =                x     
-    # dy       A      0         p1
 
     # Block cholesky
     u, v = p2, p1
@@ -76,45 +21,11 @@ def newton_direction(r_b, r_c, r_x_s, a, m, n, x, s, lu, error_check=0, use_lu=T
 
     u = u - (a.T * v) / d_2
 
-    # ldl' factorization
-    # if L and D are not provided, we calc new factorization; otherwise,
-    # reuse them
-
-    if use_lu:
-        if lu is None:
-            lu = sparse.linalg.splu(b.tocsc())
-            # wikipedia says it uses Mehrotra cholesky but the matrix i'm getting is not definite positive
-            # scikits.sparse.cholmod.cholesky fails without a warning
-
-        sol = lu.solve(rhs)
-    else:
-        sol = sparse.linalg.cg(b, rhs, tol=1e-5)[0]
-        # assert(np.max(np.abs(B*sol-rhs))<1e-5)
-
-
-    dy = sol[:m]
-    dx = sol[m : m + n]
+    dx = u
+    dy = v
     ds = -(r_x_s + s * dx) / x
 
-    AGAt = a @ sparse.diags(d_2) @ a.T
-    AGAt_inv = sparse.linalg.inv(AGAt)
-
-    import pdb; pdb.set_trace()
-
-    if error_check == 1:
-        print(
-            "error = %6.2e"
-            % (
-                norm(a.T * dy + ds + r_c)
-                + norm(a * dx + r_b)
-                + norm(s * dx + x * ds + r_x_s)
-            )
-        )
-        print("\t + err_d = %6.2e" % (norm(a.T * dy + ds + r_c)))
-        print("\t + err_p = %6.2e" % (norm(a * dx + r_b)))
-        print("\t + err_gap = %6.2e\n" % (norm(s * dx + x * ds + r_x_s)))
-
-    return dx, dy, ds, lu
+    return dx, dy, ds
 
 
 def step_size(x, s, d_x, d_s, eta=0.9995):
@@ -129,15 +40,13 @@ def mpc_sol(
     a,
     b,
     c,
+    x,
+    y,
+    s,
     max_iter=100,
     eps=1e-9,
     theta=0.9995,
     verbose=2,
-    error_check=False,
-    callback=None,
-    x = None,
-    y = None,
-    s = None
 ):
 
     a = sparse.coo_matrix(a)
@@ -155,10 +64,6 @@ def mpc_sol(
             "\n%3s %6s %9s %11s %9s %9s %9s\n"
             % ("ITER", "COST", "MU", "RESIDUAL", "ALPHAX", "ALPHAS", "MAXVIOL")
         )
-
-    # Choose initial point
-    if x is None or y is None or s is None:
-        x, y, s = initial_point(a, b, c)
 
     bc = 1 + max([norm(b), norm(c)])
 
@@ -183,17 +88,14 @@ def mpc_sol(
                 % (niter, f, mu, residual, alpha_x, alpha_s, maxviol)
             )
 
-        if callback is not None:
-            callback(x, niter)
-
         if residual < eps:
             break
 
         # ----- Predictor step -----
 
         # Get affine-scaling direction
-        dx_aff, dy_aff, ds_aff, lu = newton_direction(
-            r_b, r_c, r_x_s, a, m, n, x, s, None, error_check
+        dx_aff, dy_aff, ds_aff = newton_direction(
+            r_b, r_c, r_x_s, a, x, s
         )
 
         # Get affine-scaling step length
@@ -209,8 +111,8 @@ def mpc_sol(
         r_x_s = r_x_s + dx_aff * ds_aff - sigma * mu * np.ones((n))
 
         # Get corrector's direction
-        dx_cc, dy_cc, ds_cc, lu = newton_direction(
-            r_b, r_c, r_x_s, a, m, n, x, s, lu, error_check
+        dx_cc, dy_cc, ds_cc = newton_direction(
+            r_b, r_c, r_x_s, a, x, s
         )
 
         # Compute search direction and step
