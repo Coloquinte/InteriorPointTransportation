ae367af46c76247fe08661b4591a0c1d0b4f3204
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-01-06 16:00:17 +0100

    Working Mehrotra method from https://github.com/martinResearch/PySparseLP

--- transportation.py
+++ transportation.py
@@ -1,5 +1,242 @@
 import numpy as np
 
+from numpy.linalg import norm
+
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
+
+def initial_point(a, b, c, use_umfpack=False):
+
+    n = a.shape[1]
+    e = np.ones((n,))
+
+    # solution for min norm(s) s.t. A'*y + s = c
+    # y =sparse.linalg.cg(a*a.T, a*c,tol=1e-7)[0]
+    y = spsolve(a * a.T, a * c, use_umfpack=use_umfpack)
+
+    # y2 =sparse.linalg.cgs(A*A.T, A*c)[0]
+    # y2 =sparse.linalg.gmres(A*A.T, A*c,)[0]
+
+    s = c - a.T * y
+
+    # solution for min norm(x) s.t. Ax = b
+    x = a.T * spsolve(a * a.T, b, use_umfpack=use_umfpack)
+    # print(c.T.dot(x))
+    # x = a.T*sparse.linalg.cg(a*a.T, b,tol=1e-7)[0]
+
+    # delta_x and delta_s
+    delta_x = max(-1.5 * np.min(x), 0)
+    delta_s = max(-1.5 * np.min(s), 0)
+
+    # delta_x_c and delta_s_c
+    pdct = 0.5 * (x + delta_x * e).dot(s + delta_s * e)
+    delta_x_c = delta_x + pdct / (np.sum(s) + n * delta_s)
+    delta_s_c = delta_s + pdct / (np.sum(x) + n * delta_x)
+
+    print(
+        f"delta_x={delta_x}\ndelta_s={delta_s}\ndelta_x_c={delta_x_c}\ndelta_s_c={delta_s_c}\n"
+    )
+    # output
+    x0 = x + delta_x_c * e
+    s0 = s + delta_s_c * e
+    y0 = y
+    return x0, y0, s0
+
+
+def newton_direction(r_b, r_c, r_x_s, a, m, n, x, s, lu, error_check=0, use_lu=True):
+
+    p1 = -r_b
+    p2 = -r_c + r_x_s / x
+    rhs = np.hstack((-r_b, -r_c + r_x_s / x))
+    d_2 = -np.minimum(1e16, s / x)
+    b = sparse.vstack(
+        (
+            sparse.hstack((sparse.coo_matrix((m, m)), a)),
+            sparse.hstack((a.T, sparse.diags([d_2], [0]))),
+        )
+    )
+
+    # dy       0    A           p1
+    #     =                x     
+    # dx       At   d_2         p2
+
+
+    # dx       d_2    At        p2
+    #     =                x     
+    # dy       A      0         p1
+
+    # Block cholesky
+    u, v = p2, p1
+
+    v = v - a @ (u / d_2)
+
+    u = u / d_2
+    v = np.linalg.solve(-a @ np.diag(1.0/d_2) @ a.T, v)
+
+    u = u - (a.T * v) / d_2
+
+    # ldl' factorization
+    # if L and D are not provided, we calc new factorization; otherwise,
+    # reuse them
+
+    if use_lu:
+        if lu is None:
+            lu = sparse.linalg.splu(b.tocsc())
+            # wikipedia says it uses Mehrotra cholesky but the matrix i'm getting is not definite positive
+            # scikits.sparse.cholmod.cholesky fails without a warning
+
+        sol = lu.solve(rhs)
+    else:
+        sol = sparse.linalg.cg(b, rhs, tol=1e-5)[0]
+        # assert(np.max(np.abs(B*sol-rhs))<1e-5)
+
+
+    dy = sol[:m]
+    dx = sol[m : m + n]
+    ds = -(r_x_s + s * dx) / x
+
+    AGAt = a @ sparse.diags(d_2) @ a.T
+    AGAt_inv = sparse.linalg.inv(AGAt)
+
+    import pdb; pdb.set_trace()
+
+    if error_check == 1:
+        print(
+            "error = %6.2e"
+            % (
+                norm(a.T * dy + ds + r_c)
+                + norm(a * dx + r_b)
+                + norm(s * dx + x * ds + r_x_s)
+            )
+        )
+        print("\t + err_d = %6.2e" % (norm(a.T * dy + ds + r_c)))
+        print("\t + err_p = %6.2e" % (norm(a * dx + r_b)))
+        print("\t + err_gap = %6.2e\n" % (norm(s * dx + x * ds + r_x_s)))
+
+    return dx, dy, ds, lu
+
+
+def step_size(x, s, d_x, d_s, eta=0.9995):
+    alpha_x = -1 / min(min(d_x / x), -1)
+    alpha_x = min(1, eta * alpha_x)
+    alpha_s = -1 / min(min(d_s / s), -1)
+    alpha_s = min(1, eta * alpha_s)
+    return alpha_x, alpha_s
+
+
+def mpc_sol(
+    a,
+    b,
+    c,
+    max_iter=100,
+    eps=1e-9,
+    theta=0.9995,
+    verbose=2,
+    error_check=False,
+    callback=None,
+    x = None,
+    y = None,
+    s = None
+):
+
+    a = sparse.coo_matrix(a)
+    c = np.squeeze(np.array(c))
+    b = np.squeeze(np.array(b))
+
+    # Initialization
+
+    m, n = a.shape
+    alpha_x = 0
+    alpha_s = 0
+
+    if verbose > 1:
+        print(
+            "\n%3s %6s %9s %11s %9s %9s %9s\n"
+            % ("ITER", "COST", "MU", "RESIDUAL", "ALPHAX", "ALPHAS", "MAXVIOL")
+        )
+
+    # Choose initial point
+    if x is None or y is None or s is None:
+        x, y, s = initial_point(a, b, c)
+
+    bc = 1 + max([norm(b), norm(c)])
+
+    # Start the loop
+    niter_done = 0
+
+    for niter in range(max_iter):
+        # Compute residuals and update mu
+        r_b = a * x - b
+        r_c = a.T * y + s - c
+        r_x_s = x * s
+        mu = np.mean(r_x_s)
+        f = c.T.dot(x)
+
+        # Check relative decrease in residual, for purposes of convergence test
+        residual = norm(np.hstack((r_b, r_c, r_x_s)) / bc)
+
+        if verbose > 1:
+            maxviol = max(np.max(np.abs(r_b)), np.max(-x))
+            print(
+                "%3d %9.2e %9.2e %9.2e %9.4g %9.4g %9.2e"
+                % (niter, f, mu, residual, alpha_x, alpha_s, maxviol)
+            )
+
+        if callback is not None:
+            callback(x, niter)
+
+        if residual < eps:
+            break
+
+        # ----- Predictor step -----
+
+        # Get affine-scaling direction
+        dx_aff, dy_aff, ds_aff, lu = newton_direction(
+            r_b, r_c, r_x_s, a, m, n, x, s, None, error_check
+        )
+
+        # Get affine-scaling step length
+        alpha_x_aff, alpha_s_aff = step_size(x, s, dx_aff, ds_aff, 1)
+        mu_aff = (x + alpha_x_aff * dx_aff).dot(s + alpha_s_aff * ds_aff) / n
+
+        # Set central parameter
+        sigma = (mu_aff / mu) ** 3
+
+        # ----- Corrector step -----
+
+        # Set up right hand sides
+        r_x_s = r_x_s + dx_aff * ds_aff - sigma * mu * np.ones((n))
+
+        # Get corrector's direction
+        dx_cc, dy_cc, ds_cc, lu = newton_direction(
+            r_b, r_c, r_x_s, a, m, n, x, s, lu, error_check
+        )
+
+        # Compute search direction and step
+        dx = dx_aff + dx_cc
+        dy = dy_aff + dy_cc
+        ds = ds_aff + ds_cc
+
+        alpha_x, alpha_s = step_size(x, s, dx, ds, theta)
+
+        # Update iterates
+        x = x + alpha_x * dx
+        y = y + alpha_s * dy
+        s = s + alpha_s * ds
+
+        if niter == max_iter and verbose > 1:
+            print("max_iter reached!\n")
+        niter_done = niter
+
+    if verbose > 0:
+        print("\nDONE! [m,n] = [%d, %d], N = %d\n" % (m, n, niter))
+
+    f = c.T.dot(x)
+
+    return f, x, y, s, niter_done
+
+
 
 class TransportationMatrix:
     @staticmethod
@@ -215,7 +452,7 @@ class TransportationProblem:
     def apply_AGAt_inv(self, G, t):
         return TransportationMatrix.apply_AGAt_inv(self.N, self.M, G, t)
 
-    def solve_affine_scaling(self, beta=0.995, epsilon=1.0e-6, residual_tol=1.0e-6):
+    def solve_affine_scaling(self, beta=0.999, epsilon=1.0e-6, residual_tol=1.0e-6):
         x = self.initial_solution().flatten()
         c = self.costs.flatten()
         k = 0
@@ -239,9 +476,7 @@ class TransportationProblem:
             x = x - beta * x2 * s / np.linalg.norm(x * s)
             k += 1
 
-    def solve_dual_affine_scaling(
-        self, beta=0.995, epsilon=1.0e-6, primal_tol=1.0e-2
-    ):
+    def solve_dual_affine_scaling(self, beta=0.99, epsilon=1.0e-6, primal_tol=1.0e-2):
         y = self.initial_dual_solution().flatten()
         c = self.costs.flatten()
         b = np.concatenate((self.demands, self.capacities))[:-1]
@@ -249,21 +484,68 @@ class TransportationProblem:
         while True:
             s = c - self.apply_At(y)
             assert (s >= 0).all()
-            s_msquare = 1.0 / s**2
+            s_msquare = 1.0 / (s + 1.0e-10)**2
             d_y = self.apply_AGAt_inv(s_msquare, b)
             d_s = -self.apply_At(d_y)
-            alpha = -beta / min(min(d_s / s), -1)
+            alpha = -beta / min(d_s / s)
             y = y + alpha * d_y
             x = -s_msquare * d_s
             residual = np.dot(x, s)
             min_primal = x.min()
             value = self.value(x.reshape((self.N, self.M)))
             print(
-                f"Iter {k+1}: value {value}, residual {residual}, min primal value {min_primal}"
+                f"Iter {k+1}: value {value}, residual {residual}, min primal value {min_primal}, step {alpha}"
             )
             if min_primal >= -primal_tol and residual < epsilon:
                 return x.reshape((self.N, self.M))
             k = k + 1
+
+    def solve_mpc(self):
+        c = self.costs.flatten()
+        b = np.concatenate((self.demands, self.capacities))[:-1]
+        a = TransportationMatrix.make_dense_A(self.N, self.M)
+
+        x = self.initial_solution().flatten()
+        y = self.initial_dual_solution(1.0).flatten()
+        s = c - self.apply_At(y)
+        x = mpc_sol(a, b, c, x=x, y=y, s=s)[1]
+        #x = mpc_sol(a, b, c)[1]
+        return x.reshape((self.N, self.M))
+
+    def solve_primal_dual(self, beta=0.995, epsilon=1.0e-6):
+        x = self.initial_solution().flatten()
+        y = self.initial_dual_solution().flatten()
+        c = self.costs.flatten()
+        b = np.concatenate((self.demands, self.capacities))[:-1]
+        s = c - self.apply_At(y)
+        k = 0
+        print(f"Initial value {self.value(x.reshape((self.N, self.M)))}")
+        while True:
+            mu = np.mean(np.dot(x, s))
+            sigma = 1.0
+            vec = (x * s - mu * sigma) / s
+            # Compute y step
+            d_y = vec
+            d_y = self.apply_A(d_y)
+            d_y = -self.apply_AGAt_inv(x / s, d_y)
+            # Compute s step
+            d_s = -self.apply_At(d_y)
+            d_x = vec - x * d_s / s
+            gamma_x = max(d_x / x)
+            alpha_x = beta / gamma_x
+            gamma_s = max(d_s / s)
+            alpha_s = beta / gamma_s
+            if (s - alpha_s * d_s < 0).any():
+                import pdb; pdb.set_trace()
+            x = x - alpha_x * d_x
+            s = s - alpha_s * d_s
+            y = y - alpha_s * d_y
+            residual = np.dot(x, s)
+            print(f"Step {alpha_x}, {alpha_s}")
+            print(f"Min primal {x.min()}, min dual {s.min()}")
+            value = self.value(x.reshape((self.N, self.M)))
+            print(f"Iter {k+1}: value {value}, residual {residual}")
+            k += 1
 
     def solve_highs(self):
         import highspy
